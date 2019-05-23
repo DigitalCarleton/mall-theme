753c7c5dfb5c3ca7f3e524197fe08529969c2599
Steph <36681924+stephherbers@users.noreply.github.com>
2019-05-23 13:51:13 -0500

    change logo

--- common/footer.php
+++ common/footer.php
@@ -1,11 +1,11 @@
-    
+
     <footer>
         <div class="copy">
             <?php echo get_theme_option('footer'); ?>
         </div>
         <div class="logos">
-            <a href="http://chnm.gmu.edu"><img src="<?php echo img('rrchnm_logo.png'); ?>" alt="Logo for the Roy Rosenzweig Center for History and New Media" title="Logo for Roy Rosenzweig Center for History and New Media"></a>
-            <a href="http://neh.gov"><img src="<?php echo img('neh_logo.png'); ?>" alt="Logo for the National Endowment for the Humanities"></a>
+            <a href="https://www.carleton.edu/"><img src="<?php echo img('Carleton_simplified_CMYK_Blue.eps'); ?>" alt="Logo for Carleton College" title="Carleton College"></a>
+            <a href="https://www.carleton.edu/"><img src="<?php echo img('Carleton_word_mark_CMYK_Blue.eps'); ?>" alt="Logo for Carleton College"></a>
         </div>
         <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
     </footer>
