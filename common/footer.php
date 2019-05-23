ed0b55e7d9b0f629f3bfe80c2de9de828602057f
Steph <36681924+stephherbers@users.noreply.github.com>
2019-05-23 13:53:35 -0500

    Update footer.php

--- common/footer.php
+++ common/footer.php
@@ -4,8 +4,8 @@
             <?php echo get_theme_option('footer'); ?>
         </div>
         <div class="logos">
-            <a href="https://www.carleton.edu/"><img src="<?php echo img('Carleton_simplified_CMYK_Blue.eps'); ?>" alt="Logo for Carleton College" title="Carleton College"></a>
-            <a href="https://www.carleton.edu/"><img src="<?php echo img('Carleton_word_mark_CMYK_Blue.eps'); ?>" alt="Logo for Carleton College"></a>
+            <a href="https://www.carleton.edu/"><img src="<?php echo img('Carleton_simplified_CMYK_Blue.jpg'); ?>" alt="Logo for Carleton College" title="Carleton College"></a>
+            <a href="https://www.carleton.edu/"><img src="<?php echo img('Carleton_word_mark_CMYK_Blue.jpg'); ?>" alt="Logo for Carleton College"></a>
         </div>
         <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
     </footer>
