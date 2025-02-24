e1c2ffab7661f59ef7692d3a3667ccbb907a73ee
Clark Gibson Glymph <glymphc@carleton.edu>
2025-02-24 13:25:24 -0600

    ajust header to column

--- common/header.php
+++ common/header.php
@@ -40,9 +40,11 @@
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
     <header>
-        <h1 id="site-title"><?php echo link_to_home_page(theme_logo(), array('alt' => 'Logo for Carleton Guide to Medieval Rome', 'title' => 'Logo for Carleton Guide to Medieval Rome')); ?></h1>
+        <div class="row">
+            <div class="column"><h1 id="site-title"><?php echo link_to_home_page(theme_logo(), array('alt' => 'Logo for Carleton Guide to Medieval Rome', 'title' => 'Logo for Carleton Guide to Medieval Rome')); ?></h1></div>
+            <div class="column"><?php echo search_form(); ?></div>
+        </div>
         
-        <?php echo search_form(); ?>
         
         <nav id="navigation" data-role="none">
             <?php echo public_nav_main(); ?>
